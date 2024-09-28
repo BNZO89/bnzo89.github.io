cb5d81c47a58d47959ab9697a0601c7e8fb752d1
duguayworld <duguay_world@outlook.com>
2024-09-28 18:41:34 -0400

    last-update

--- scripts/changeline.sh
+++ scripts/changeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the directory where your HTML files are located
-DIRECTORY="/home/paulgrey/Templates/electricitedj"
+DIRECTORY="/home/paulgrey/Templates/bnzo89.github.io/bnzo89.github.io"
 
 # Find and replace in all .html files
-find "$DIRECTORY" -type f -name "*.html" -exec sed -i "s/parlerabout your next construction project/pour nous parler de votre prochain projet de construction/g" {} +
+find "$DIRECTORY" -type f -name "*.html" -exec sed -i 's/class="menu-cta">Commencer un Projet<\/a>/href="index.html%3Fp=15.html" class="menu-cta">Commencer un Projet<\/a>/g' {} +
 
 # Print a message after completion
 echo "All Replacements complete!"
