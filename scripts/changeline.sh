fb3ba28a3ea9fced5c3324157bf5400fc474240c
duguayworld <duguay_world@outlook.com>
2024-09-28 18:58:24 -0400

    fucking-commit-editing I commited some garbage

--- scripts/changeline.sh
+++ scripts/changeline.sh
@@ -4,7 +4,7 @@
 DIRECTORY="/home/paulgrey/Templates/bnzo89.github.io/bnzo89.github.io"
 
 # Find and replace in all .html files
-find "$DIRECTORY" -type f -name "*.html" -exec sed -i 's/class="menu-cta">Commencer un Projet<\/a>/href="index.html%3Fp=15.html" class="menu-cta">Commencer un Projet<\/a>/g' {} +
+find "$DIRECTORY" -type f -name "*.html" -exec sed -i 's/class="menu-cta">Commencer un Projet<\/a>/href="index.html?p=15.html" class="menu-cta">Commencer un Projet<\/a>/g' {} +
 
 # Print a message after completion
 echo "All Replacements complete!"
